1c2664bc01cccecc51a888798afafb2dcbaf962f
cuberolopez96 <cuberolopez96@gmail.com>
2017-02-13 11:13:46 +0100

    init

---
+++ modules/functions.php
@@ -0,0 +1,35 @@
+<?php 
+function cleanInput($input) {
+ 
+  $search = array(
+    '@<script[^>]*?>.*?</script>@si',   // Elimina javascript
+    '@<[\/\!]*?[^<>]*?>@si',            // Elimina las etiquetas HTML
+    '@<style[^>]*?>.*?</style>@siU',    // Elimina las etiquetas de estilo
+    '@<![\s\S]*?--[ \t\n\r]*>@'         // Elimina los comentarios multi-línea
+  );
+ 
+    $output = preg_replace($search, '', $input);
+    return $output;
+  }
+function uploadFile($file){
+	$dir = 'resources/uploads/';
+	$mymeTypes = array('video/mp4','video/x-flv','audio/mpeg3','audio/x-mpeg3','image/jpeg','image/pjpeg','image/png');
+	
+
+		if(!in_array($file['type'], $mymeTypes)){
+			throw new Exception("El fichero no tiene una extension permitida");
+		}else{
+			$dirname = $dir.basename($file['name']);
+			if (!move_uploaded_file($file['tmp_name'], $dirname)) {
+				throw new Exception("No se pudo subir el fichero", 1);
+				
+			}else{
+			return $dirname;
+			}
+
+		}
+	
+
+}
+  
+?>
---
+++ resources/templates/main.php
@@ -0,0 +1,14 @@
+<?php 
+$preguntas = Preguntas::getPreguntas();
+
+ ?>
+<div class="cards">
+	<?php foreach ($preguntas as $key => $pregunta): ?>
+		<div class="card">
+			<header>
+				<h3><?php echo $pregunta['pregunta']; ?><a href="/?page=editpregunta&id=<?php echo $pregunta['id'] ?>"><i class="fa fa-pencil"></i></a></h3>
+				<h4><a href="/?page=pregunta&id=<?php echo $pregunta['id'] ?>">Ver</a></h4>
+			</header>
+		</div>
+	<?php endforeach ?>
+</div>
